d029045959d632bdf8c9b3f87d11acb922aba856
Sumitsoni21 <128276751+Sumitsoni21@users.noreply.github.com>
2025-07-05 00:31:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createRoot } from "react-dom/Client";
 
+function Msg() {
+  return <p> I am learning React. My life is getting better. </p>;
+}
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement =  doucumet.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<Msg />);
